905b3abd08784e9e17509b548f8bfd998f912729
Giovanni Avelar <geovanny.avelar@gmail.com>
2025-10-12 13:41:19 -0300

    Fix page table start address and update page size definition

--- mem.h
+++ mem.h
@@ -1,12 +1,8 @@
 #define _PHYS_MEM_START   0x0100
 #define _PHYS_MEM_END     0x08FF
-#define _PAGE_TABLE_START _PHYS_MEM_START + 1024
+#define _PAGE_TABLE_START _PHYS_MEM_START + 512
 #define _PAGE_TABLE_END   _PAGE_TABLE_START + 20
-#define _PAGE_SIZE        64
-
-struct mem_block {
-  struct mem_block *next;
-} mem;
+#define _PAGE_SIZE        128
 
 struct pte {
   unsigned int used;
